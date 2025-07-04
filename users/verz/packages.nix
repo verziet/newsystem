5790ced8b4d23a7ec95a7e3e564d555b5af4039a
verziet <garlic-boss-batch@duck.com>
2025-07-04 19:14:00 +0200

    feat: add gemini-cli package and stremio package

--- users/verz/packages.nix
+++ users/verz/packages.nix
@@ -1,9 +1,13 @@
 {
+	pkgs,
   inputs,
   host,
   ...
 }: {
   home.packages = [
     inputs.zen-browser.packages."${host.system}".default
-  ];
+  ] ++ with pkgs; [
+		gemini-cli
+		stremio
+	];
 }
