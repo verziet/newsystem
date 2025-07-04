d9ef3d32c83accc919422f8365828e35179d605b
verziet <garlic-boss-batch@duck.com>
2025-07-04 19:20:26 +0200

    fix: verz packages.nix

--- users/verz/packages.nix
+++ users/verz/packages.nix
@@ -1,13 +1,15 @@
 {
-	pkgs,
+  pkgs,
   inputs,
   host,
   ...
 }: {
-  home.packages = [
-    inputs.zen-browser.packages."${host.system}".default
-  ] ++ with pkgs; [
-		gemini-cli
-		stremio
-	];
+  home.packages =
+    [
+      inputs.zen-browser.packages."${host.system}".default
+    ]
+    ++ (with pkgs; [
+      gemini-cli
+      stremio
+    ]);
 }
